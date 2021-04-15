b3a7cbd19c2b0350218c97b506bc189a6969a87a
masudhajari <azharc614@gmail.com>
2021-04-15 12:27:25 +0600

    welcome page remove add website page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\About;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,6 +11,7 @@ class HomeController extends Controller
     public function index() 
     {
         $about = About::first();
-        return view('welcome', compact('about'));
+        $categories = Category::all();
+        return view('website', compact('about', 'categories'));
     }
 }
