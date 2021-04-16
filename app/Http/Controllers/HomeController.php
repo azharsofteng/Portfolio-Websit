cf028e2be115b7a880d7d23b69777011128152c8
masudhajari <azharc614@gmail.com>
2021-04-16 16:00:22 +0600

    servie data daynamically show in website and resume model and controlle create

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\About;
 use App\Models\Category;
 use App\Models\Portfolio;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,6 +15,7 @@ class HomeController extends Controller
         $about = About::first();
         $categories = Category::all();
         $portfolios = Portfolio::latest()->get();
-        return view('website', compact('about', 'categories', 'portfolios'));
+        $services = Service::latest()->get()->take(4);
+        return view('website', compact('about', 'categories', 'portfolios', 'services'));
     }
 }
